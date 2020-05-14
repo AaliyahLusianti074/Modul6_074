98bd8f11740201e555f5a614353f7cd654f11987
AaliyahLusianti074 <aliyahlsnt@gmail.com>
2020-05-14 16:27:24 +0700

    first commit

---
+++ app/src/main/java/com/example/modul6/Konfigurasi.java
@@ -0,0 +1,34 @@
+package com.example.modul6;
+
+public class Konfigurasi {
+    //PENTING! JANGAN LUPA GANTI IP SESUAI DENGAN IP KOMPUTER DIMANA DATA PHP BERADA
+public static final String URL_ADD="http://192.168.217.1/modul6mobile/insert.php";
+public static final String URL_GET_ALL = "http://192.168.217.1/modul6mobile/read.php";
+public static final String URL_GET_MHS = "http://192.168.217.1/modul6mobile/select.php?id=";
+public static final String URL_UPDATE_MHS = "http://192.168.217.1/modul6mobile/update.php";
+public static final String URL_DELETE_MHS = "http://192.168.217.1/modul6mobile/delete.php?id=";
+
+//Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Script PHP
+public static final String KEY_MHS_ID = "id";
+public static final String KEY_MHS_NAMA = "nama";
+public static final String KEY_MHS_JURUSAN = "jurusan";
+public static final String KEY_MHS_EMAIL = "email";
+public static final String KEY_MHS_ALAMAT = "alamat";
+public static final String KEY_MHS_TELEPON = "telepon";
+public static final String KEY_MHS_IMAGE = "image";
+
+//JSON Tags
+public static final String TAG_JSON_ARRAY="result";
+public static final String TAG_ID = "id";
+public static final String TAG_NAMA = "nama";
+public static final String TAG_JURUSAN = "jurusan";
+public static final String TAG_EMAIL = "email";
+public static final String TAG_ALAMAT = "alamat";
+public static final String TAG_TELEPON = "telepon";
+
+
+//ID Mahasiswa
+// mhs itu singkatan dari Mahasiswa
+public static final String MHS_ID = "mhs_id";
+
+}
